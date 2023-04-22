e576110e3ccc76523682b51aeafda436749e4821
Sam Bilbow <samuel.bilbow@icloud.com>
2023-04-22 12:48:39 +0100

    :sparkles: (gist.rb) Add rate-limit check to gist plugin

--- _plugins/gist.rb
+++ _plugins/gist.rb
@@ -1,32 +1,36 @@
 module Jekyll
-    class GistTag < Liquid::Tag
-      def initialize(tag_name, input, tokens)
-        super
-        @gist_id = input.strip
+  class GistTag < Liquid::Tag
+    def initialize(tag_name, input, tokens)
+      super
+      @gist_id = input.strip
+    end
+
+    def render(context)
+      uri = URI("https://api.github.com/gists/#{@gist_id}")
+      response = Net::HTTP.get(uri)
+
+      if response.include?("API rate limit exceeded")
+        return "`Error: API rate-limited by GitHub, follow link instead`"
       end
-  
-      def render(context)
-        uri = URI("https://api.github.com/gists/#{@gist_id}")
-        response = Net::HTTP.get(uri)
-        json = JSON.parse(response)
-  
-        # Extract the first file from the gist
-        filename, file = json['files'].first
-  
-        # Capture the file contents as a string
-        captured = Liquid::Template.parse("{% capture gist_content %}#{file['content']}{% endcapture %}").render(context)
-  
-        # Build the code block with the captured contents and custom data attribute
-        code_block = Liquid::Template.parse("{% highlight #{file['language']} %}{{ gist_content }}{% endhighlight %}").render(context)
-  
-        # Wrap the code block in a div with the correct classes and custom data attribute
-        code_block = "<div class='language-#{file['language'].downcase} highlighter-rouge' data-gist-url='https://gist.github.com/#{json['owner']['login']}/#{@gist_id}'>#{code_block}</div>"
-  
-        # Return the generated code block
-        code_block
-      end
+
+      json = JSON.parse(response)
+
+      # Extract the first file from the gist
+      filename, file = json['files'].first
+
+      # Capture the file contents as a string
+      captured = Liquid::Template.parse("{% capture gist_content %}#{file['content']}{% endcapture %}").render(context)
+
+      # Build the code block with the captured contents and custom data attribute
+      code_block = Liquid::Template.parse("{% highlight #{file['language']} %}{{ gist_content }}{% endhighlight %}").render(context)
+
+      # Wrap the code block in a div with the correct classes and custom data attribute
+      code_block = "<div class='language-#{file['language'].downcase} highlighter-rouge' data-gist-url='https://gist.github.com/#{json['owner']['login']}/#{@gist_id}'>#{code_block}</div>"
+
+      # Return the generated code block
+      code_block
     end
   end
-  
-  Liquid::Template.register_tag('gist', Jekyll::GistTag)
-  
+end
+
+Liquid::Template.register_tag('gist', Jekyll::GistTag)
